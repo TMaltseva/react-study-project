cdaa25c77c8ba343e7293fa192639d1b826047f5
TMaltseva <bacardeonie@gmail.com>
2024-07-02 20:34:59 +0300

    chore: add necessary vite and eslint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,6 +7,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
+    'plugin:react/jsx-runtime',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs', '**/*.d.ts'],
   parser: '@typescript-eslint/parser',
